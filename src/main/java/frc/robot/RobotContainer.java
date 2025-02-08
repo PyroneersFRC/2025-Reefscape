2472eef3e7aaaa488be6680fbc2b0eb3d85392b4
DivappThePlug <147906478+DivappThePlug@users.noreply.github.com>
2025-02-08 13:10:42 +0200

    add elevator subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,13 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.MathUtil;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Commands.autoAlignCmd;
+import frc.robot.Constants.CANids;
 import frc.robot.Constants.xboxConstants;
 import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -25,7 +27,8 @@ public class RobotContainer {
   private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
   private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
   private final CommandXboxController m_driverController = new CommandXboxController(Constants.xboxConstants.kDriverControllerPort);
-  private final Command m_autoAlign = new autoAlignCmd(m_driveSubsystem,m_visionSubsystem);
+  // private final Command m_autoAlign = new autoAlignCmd(m_driveSubsystem,m_visionSubsystem);
+  private final ElevatorSubsystem  m_elevatorSubsystem = new ElevatorSubsystem(CANids.kRightElevatorCanId, CANids.kLeftElevatorCanId);
 
   //public RobotContainer() {
   //     swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
@@ -54,7 +57,11 @@ public class RobotContainer {
 
   private void configureButtonBindings() {
       m_driverController.b().onTrue(m_driveSubsystem.resetGyro());
-      m_driverController.a().whileTrue(m_driveSubsystem.runalign());
+      //m_driverController.a().whileTrue(m_driveSubsystem.runalign());
+      m_driverController.leftBumper().onTrue(m_elevatorSubsystem.decrease());
+      m_driverController.rightBumper().onTrue(m_elevatorSubsystem.accelerate());
+      m_driverController.y().onTrue(m_elevatorSubsystem.runElevator());
+      m_driverController.x().onTrue(m_elevatorSubsystem.stopElevator());
   }
 
 
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,25 +1,50 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANids;
 import frc.robot.Constants.robot;
 
 
 public class ElevatorSubsystem  extends SubsystemBase{
-    private final  SparkMax m_rightSparkMax;
-    private final  SparkMax m_leftSparkMax;
+    private final SparkMax m_rightMotor;
+    private final SparkMax m_leftMotor;
+    private final AbsoluteEncoder m_encoder;
     private final PIDController m_elevatorPIDController;
+    private double m_currentSpeed;
 
     public ElevatorSubsystem(int rightMotorID, int leftMotorID){
-        m_leftSparkMax = new SparkMax(leftMotorID, MotorType.kBrushed);
-        m_rightSparkMax = new SparkMax(leftMotorID, MotorType.kBrushed);
+        m_leftMotor = new SparkMax(leftMotorID, MotorType.kBrushed);
+        m_rightMotor = new SparkMax(rightMotorID, MotorType.kBrushed);
+
+        m_encoder = m_rightMotor.getAbsoluteEncoder();
+            
 
         m_elevatorPIDController = robot.kPIDElevator;
-        
+        m_currentSpeed = 0;
+    }
+
+    public void setVoltage() {
+        m_leftMotor.set(m_currentSpeed);
+        m_rightMotor.set(-m_currentSpeed);
+    }
+
+    public Command runElevator(){
+        return this.run(this::setVoltage);
+    }
+    public Command stopElevator(){
+        return this.runOnce(() ->m_currentSpeed = 0);
+    }
+    public Command accelerate(){
+        return this.runOnce(() ->m_currentSpeed += 0.1);
+    }
+    public Command decrease(){
+        return this.runOnce(() ->m_currentSpeed -= 0.1);
     }
 }
